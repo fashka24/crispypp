e4181797b855198323f7a40a9e4694fccd0e0eec
fashka24 <sakirovdavyd@gmail.com>
2025-04-18 13:08:17 +0300

    Add ctypes

---
+++ example/ctypes.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "../crispypp/ctypes.hpp"
+
+int main() {
+    crs::c_variable my_var;
+
+    my_var.is_const  = true;
+    my_var.is_ptr    = true;
+    my_var.type_name = C_TYPE_DOUBLE;
+    my_var.var_name  = "my_var";
+    my_var.var_value = "12.7";
+
+    std::cout
+    << crs::build_c_var(my_var) // const double * my_var = 12.7;
+    << "\n";
+}
